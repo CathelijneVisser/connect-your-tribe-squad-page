08a7c5ba908ed007d71b0074b6203237e135d382
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-02-17 15:58:02 +0100

    wip

--- public/script.js
+++ public/script.js
@@ -1,21 +1,15 @@
 let button = document.querySelector(".freeze");
 let articles = document.querySelectorAll("article");
-let li = document.querySelectorAll("li");
 
-
+    
 button.addEventListener("click", stop);
 
-li.forEach((list) => {
-    link.addEventListener("click",focus);
-})
 
 
 function stop () {
+    
     articles.forEach(article => {
         article.classList.toggle("pause")
     });
 }
 
-function focus () {
-    button.focus()
-}
