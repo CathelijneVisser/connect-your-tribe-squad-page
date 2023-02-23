cfce7d7fb33f3cd0df68118c14f6da215ecc3abb
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-02-23 10:03:38 +0100

    test

--- index.js
+++ index.js
@@ -38,15 +38,6 @@ app.get('/', (request, response) => {
   })
 })
 
-// Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8000)
-
-// Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
-
 app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
